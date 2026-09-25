daeeef0021a130d2a33a312658ec40fd4972750d
newly.dev <api@newly.dev>
2026-09-25 19:38:16 +0000

    chore: Re-enable TEST_MODE for admin test login button

--- constants/testMode.ts
+++ constants/testMode.ts
@@ -3,7 +3,7 @@
  * Ligue/desligue aqui. Quando false:
  *   - o botão "Entrar como Admin Geral (teste)" some da tela de login
  */
-export const TEST_MODE = false;
+export const TEST_MODE = true;
 
 export const TEST_ADMIN_EMAIL = "admingeral@gmail.com";
 export const TEST_ADMIN_PASSWORD = "123456";
